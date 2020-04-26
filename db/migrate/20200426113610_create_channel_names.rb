793b973361c77aca729324b7233ca3c947d3d68a
Kajal Priyadarshani <kajal@optimaltransnational.com.au>
2020-04-26 19:47:53 +0530

    minor change in migration

--- db/migrate/20200426113610_create_channel_names.rb
+++ db/migrate/20200426113610_create_channel_names.rb
@@ -1,7 +1,8 @@
 class CreateChannelNames < ActiveRecord::Migration[6.0]
   def change
     create_table :channel_names do |t|
-
+      t.string :name
+      t.integer :tvshow_id
       t.timestamps
     end
   end
